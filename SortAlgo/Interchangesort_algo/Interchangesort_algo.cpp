54e2de04699a3351a255cf4992517b3a46f2add4
Quy1314 <24521494@gm.uit.edu.vn>
2025-08-31 23:21:42 +0700

    Upload SortAlgo

---
+++ SortAlgo/Interchangesort_algo/Interchangesort_algo.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void InterchangeSort(int arr[], int n);
+
+int main() {
+	srand(time(0));
+	int n = 0;
+	n = rand() % 100 + 1;
+	int arr[1000];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
+
+	cout << "Original array: ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	InterchangeSort(arr, n);
+	cout << "\nSorted array: ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+}
+
+void InterchangeSort(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+}
