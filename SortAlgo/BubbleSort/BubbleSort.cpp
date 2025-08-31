02ffc390e40afb504dc018ae172b7fd4dcc68e31
Quy1314 <24521494@gm.uit.edu.vn>
2025-09-01 01:36:11 +0700

    Update bubble sort

---
+++ SortAlgo/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void BubbleSort(int arr[], int n);
+
+int main() {
+	int n = 0;
+	srand(time(0));
+	n = 10;
+	int arr[1000];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
+	cout << "Original array: ";
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	BubbleSort(arr, n);
+
+	cout << "\nSorted array: ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	
+}
+
+void BubbleSort(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
---
+++ SortAlgo/SelectionSort/SelectionSort.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <ctime>	
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+void SelectionSort(vector<int>& arr, int n);
+
+int main() {
+	int n = 0;
+	srand(time(0));
+	n = rand() % 100 + 1;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
+	cout << "Original array: ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	SelectionSort(arr, n);
+	cout << "\nSorted array: ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+void SelectionSort(vector<int>& arr, int n) {
+	for (int i = 0; i < n - 1; i++) {
+		int minIndex = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
+		if (minIndex != i) {
+			swap(arr[i], arr[minIndex]);
+		}
+	}
+}
