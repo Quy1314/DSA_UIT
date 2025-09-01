bba8810546da0658976077e48e685c5b33883788
Quy1314 <24521494@gm.uit.edu.vn>
2025-09-02 02:08:05 +0700

    Update QuickSort

---
+++ SortAlgo/QuickSort/QuickSort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> QuickSort(vector<int>& arr);
+
+int main() {
+	vector<int> arr = { 34, 7, 23, 32, 5, 62 };
+	int n = arr.size();
+	QuickSort(arr);
+	for(int x:arr){
+		cout << x << " ";
+	}
+	arr = QuickSort(arr);
+
+	cout << "\nSorted array: ";
+	for(int x:arr){
+		cout << x << " ";
+	}
+	return 0;
+}
+
+vector<int> QuickSort(vector<int>& arr) {
+	if (arr.size() <= 1) return arr;
+	int pivot = arr.back();
+	vector<int> left, right;
+	for (int i = 0; i < arr.size() - 1; i++) {
+		if (arr[i] < pivot) left.push_back(arr[i]);
+		else right.push_back(arr[i]);
+	}
+	left = QuickSort(left);
+	right = QuickSort(right);
+	left.push_back(pivot);
+	left.insert(left.end(), right.begin(), right.end());
+	return left;
+}
---
+++ SortAlgo/QuickSortxPartition/QuickSortxPartition.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int Partition(vector<int>& arr, int low, int high);
+void QuickSort(vector<int>& arr, int low, int high);
+
+int main() {
+	vector<int> arr = { 34, 7, 23, 32, 5, 62 };
+	int n = arr.size();
+	for (int x : arr) {
+		cout << x << " ";
+	}
+	QuickSort(arr, 0, n-1);
+
+	cout << "\nSorted array: ";
+	for (int x : arr) {
+		cout << x << " ";
+	}
+	return 0;
+}
+
+int Partition(vector<int>& arr, int low, int high) {
+	int pivot = arr[low];
+	int i = low;
+	int j = high;
+	while (i < j) {
+		do {
+			i++;
+		} while (arr[i] <= pivot);
+		do  {
+			j--;
+		} while (arr[j] > pivot);
+		if (i < j) {
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[low], arr[j]);
+	return j;
+}
+
+
+void QuickSort(vector<int>& arr, int low, int high) {
+	if (low < high) {
+		int pivot = Partition(arr, low, high);
+		QuickSort(arr, low, pivot - 1);
+		QuickSort(arr, pivot + 1, high);
+	}
+}
